0e65acdf7cfe77fa9ca50e934685049fb54b01ba
maxhakin <maxtwh@hotmail.co.uk>
2023-11-30 13:12:13 +0100

    Update the tilemap and implement a game cam for better control of the game view

--- StepQuest/GameScene.swift
+++ StepQuest/GameScene.swift
@@ -9,26 +9,20 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+
     private var inputHandler: InputHandler!
     private var initialTouchLocation: CGPoint?
     private var tileMap: SKTileMapNode?
     private var gameCam: SKCameraNode?
     
     override func didMove(to view: SKView) {
-        
-        self.gameCam = SKCameraNode()
-        self.addChild(gameCam!)
-        self.camera = gameCam
-
-        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
-        view.addGestureRecognizer(pinchGesture)
-        super.didMove(to: view)
         loadTileMap()
+        self.gameCam = self.childNode(withName: "gameCam") as? SKCameraNode 
+        self.camera = gameCam
+        super.didMove(to: view)
         
     }
+    
     
     func loadTileMap() {
         guard let tileMapNode = self.childNode(withName: "terrainMap") as? SKTileMapNode else {
@@ -37,49 +31,16 @@ class GameScene: SKScene {
                 tileMap = tileMapNode
     }
     
-    @objc func handlePinch(_ sender: UIPinchGestureRecognizer) {
-            guard let camera = gameCam else { return }
-
-            if sender.state == .changed {
-                let scale = 1 / sender.scale
-                camera.setScale(camera.xScale * scale)
-                sender.scale = 1.0
-
-                // Add logic here to clamp the camera's scale to prevent over-zooming
-            }
-    }
-    
-    func clampCameraScale() {
-        guard let camera = gameCam else { return }
-        let minScale: CGFloat = 1.0 // Minimum zoom level
-        let maxScale: CGFloat = 2.0 // Maximum zoom level
-
-        camera.xScale = max(minScale, min(camera.xScale, maxScale))
-        camera.yScale = camera.xScale // Keep the scale uniform
-    }
-    
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
